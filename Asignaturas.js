1f608ac4d58d6f2f2f23d097f8f1e1113457e338
miguelpaulino239@gmail.com <miguelpaulino239.com>
2022-02-26 22:08:25 -0500

    Validatio for promt of signatures

---
+++ Asignaturas.js
@@ -0,0 +1,34 @@
+let asignaturas = ["Matematica", "Algebra", "Programacion POO", "Logica", "Ingeneria de sistemas", "Calculo", "Programacion Phyton", "Programacion Php", "Git y Github", "Scrum", "Marketin Digital", "Ingles"];
+let profesores = ["Alberto Perez", "Rodrigo Martinez", "Pablo Fench", "Lixandra Labandera","Marcos Piotosi", "Maritza Cabello"];
+let alumnos = ["Cofla Rompepelotas", "Miguel Paulino", "Daniela Marquez"];
+let preguntaAsignatura = prompt("Desea incscribirse en las asignaturas");
+
+const converTString = (a) =>{
+    switch(a.toLowerCase()){
+        case "si": case "yes": case "afirmative":case "afirmativo": return true;
+        case "no": case "negative": case "negativo": return false;
+        default: return Boolean(a);
+    }
+}
+const converTStringSignatures = (b) =>{
+    switch(b.toLowerCase()){
+        case "no": return false;
+        default: return true;
+    }
+}
+const validationQuest = (c) =>{
+    let boleanQuestion = converTString(c)
+    if (boleanQuestion == true){
+        let arraysSignatures = prompt("Escribe una asignatura a cursar, en caso de no querer escriba 'no'.");
+        let boleanSignatures = converTStringSignatures(arraysSignatures);
+        console.log(boleanSignatures);
+        while(boleanSignatures == true){
+            arraysSignatures = prompt("Escribe una asignatura a cursar, en caso de no querer escriba 'no'.");
+            boleanSignatures = converTStringSignatures(arraysSignatures);
+        }
+    }else{
+        alert("Muchas Gracias, puede irse.")
+    }
+}
+
+validationQuest(preguntaAsignatura);
